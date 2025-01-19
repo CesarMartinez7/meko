89ed4045019ceb0ba31d107452c7b4a18949f327
cesarubuntu <cesarwamartinez@gmail.com>
2025-01-19 14:11:38 -0500

    se añade todo bien

--- src/Pages/Anime.tsx
+++ src/Pages/Anime.tsx
@@ -17,18 +17,26 @@ export default function Manga() {
   }, [id]);
   return (
     <div className="">
-      <div className="w-full h-[30vh] bg-gradient-to-b  bg-stone-950">
-      </div>
+      <div className="w-full h-[30vh] bg-gradient-to-b  bg-stone-950"></div>
       <div className="p-10 rounded-lg top-48 absolute ">
         <div className="bg-transparent w-64">
-          <img src={anime?.images.jpg.large_image_url} alt="" className="rounded-2xl shadow-md w-full h-full"/>
+          <img
+            src={anime?.images.jpg.large_image_url}
+            alt=""
+            className="rounded-2xl shadow-md w-full h-full"
+          />
         </div>
         <p className="font-extralight">{anime?.title_japanese}</p>
         <h3 className="font-semibold text-7xl">{anime?.title}</h3>
         <span className="font-extralight text-sm">{anime?.duration}</span>
-        <button className="button">
-          <Icon icon="tabler:play" width="20" height="20" />
+        <button className="btn glass btn-wide border">
+          {" "}
+          <Icon icon="solar:play-line-duotone" width="24" height="24" />
           Play
+        </button>
+        <button className="btn btn-circle">
+          <Icon icon="solar:star-line-duotone" width="24" height="24" />
+          {anime?.score}
         </button>
         <p>{anime?.score}</p>
         <p>{anime?.synopsis}</p>
