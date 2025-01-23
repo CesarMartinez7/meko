4bb656138c9d623d498c579a6bac1f410f365b3b
Cesar <cesarwamartinez@gmail.com>
2025-01-23 00:15:36 -0500

    se mejora la interfaz de theme de view anime

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -28,9 +28,9 @@ function Grid({url, text = "Tus resultados"} : GridProps){
     }
 
     return(
-        <div className="p-4 md:p-8 rounded-2xl ">
+        <div className="p-4 md:p-12 rounded-2xl ">
             <h3 className="font-medium">{text}</h3>
-            <section className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-10  gap-2 ">
+            <section className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-7 2xl:grid-cols-10  gap-2 ">
                 {data && data.map((item) => (
                     <HoverCard item={item} key={item.mal_id}></HoverCard>
                 ))}
--- src/Pages/ViewAnime.tsx
+++ src/Pages/ViewAnime.tsx
@@ -5,7 +5,7 @@ import { Chapters } from "../Types/Chapter";
 export default function ViewAnime() {
   const [data, setData] = useState<Array<Chapters>>([]);
   const [chapter, setChapter] = useState<number>(1);
-  const [isDub,setIsDub] = useState(true)
+  const [isDub, setIsDub] = useState(true);
   const { id, name } = useParams();
 
   useEffect(() => {
@@ -20,33 +20,48 @@ export default function ViewAnime() {
       <div className="bg-black w-full md:h-screen">
         <iframe
           className="w-full md:h-screen"
-          src={`https://vidsrc.cc/v2/embed/anime/ani${id}/${chapter}/${isDub ? "dub" : "sub"}`}
+          src={`https://vidsrc.cc/v2/embed/anime/ani${id}/${chapter}/${
+            isDub ? "dub" : "sub"
+          }`}
           frameBorder="0"
           allowFullScreen
         ></iframe>
       </div>
       <div className="p-5 lg:p-12 h-screen overflow-y-auto">
         <section>
-          <h3 className="font-bold text-3xl bg-gradient-to-br from-slate-900 to-zinc-500 bg-clip-text text-transparent">
+          <h3 className="font-bold text-3xl ">
+            <p className="text-[13px] font-extralight">Episodio {chapter}</p>
             {name?.toUpperCase().slice(1, name?.length)}
           </h3>
-          <p className="font-light">Episodio {chapter}</p>
+          <p className="text-[13px]">{data[chapter]?.title}</p>
+          <p className="text-[13px]">{data[chapter]?.title_japanese}</p>
+
           <div className="flex gap-2 mb-4">
-            <button className="btn btn-info" onClick={() =>{
-              setIsDub(true)
-            } }>Dub</button>
-            <button className="btn-info btn" onClick={() =>{
-              setIsDub(false)
-            } }>Sub</button>
-            <button className="btn btn-sm btn-neutral">Vidlink</button>
-            <button className="btn btn-sm btn-neutral">Vidsrc</button>
+            <button
+              className="btn btn-sm"
+              onClick={() => {
+                setIsDub(true);
+              }}
+            >
+              Dub
+            </button>
+            <button
+              className="btn btn-sm"
+              onClick={() => {
+                setIsDub(false);
+              }}
+            >
+              Sub
+            </button>
+            <button className="btn btn-sm ">Vidlink</button>
+            <button className="btn btn-sm ">Vidsrc</button>
           </div>
         </section>
         <ul className="grid grid-cols-5 lg:grid-cols-10 gap-2 ">
           {Array.from({ length: data.length }, (_, i) => (
             <li
               key={i}
-              className="glassMorf p-2 text-center border hover:cursor-pointer"
+              className="kbd"
               onClick={() => {
                 setChapter(i + 1);
               }}
