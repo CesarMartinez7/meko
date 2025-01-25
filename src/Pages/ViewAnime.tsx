819094b7f72653912eacfe2b11c0b18f958f0daf
Cesar ubuntu <cesarwamartinez@gmail.com>
2025-01-24 23:25:34 -0500

    Mejoro la interfaz de la vista de dragon ball

--- src/Pages/ViewAnime.tsx
+++ src/Pages/ViewAnime.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Chapters } from "../Types/Chapter";
 
-interface PropsParams extends Record<string,string | undefined> {
+interface PropsParams extends Record<string, string | undefined> {
   id: string;
   name: string;
   caps: string;
@@ -13,7 +13,6 @@ export default function ViewAnime() {
   const [chapter, setChapter] = useState<number>(1);
   const [isDub, setIsDub] = useState(true);
   const { id, name, caps } = useParams<PropsParams>();
-
 
   const numericId = id ? parseInt(id, 10) : 0;
   const numericCaps = caps ? parseInt(caps, 10) : 0;
@@ -38,33 +37,40 @@ export default function ViewAnime() {
         ></embed>
       </div>
       <div className="p-5 lg:p-12 h-screen overflow-y-auto">
-        <section>
-          <h3 className="font-bold text-3xl ">
-            <p className="text-[13px] font-extralight">Episodio {chapter}</p>
-            {name}
-          </h3>
+        <section className="text-center">
+          <p className="text-[11px]">Estas viendo el episodio {chapter}</p>
+          <h3 className="font-bold text-3xl"> {name}</h3>
+          <p className="text-[13px] font-extralight"> </p>
           <p className="text-[13px]">{data[chapter]?.title}</p>
-          <p className="text-[13px]">{data[chapter]?.title_japanese}</p>
-
-          <div className="flex gap-2 mb-4">
-            <button
-              className="btn btn-sm"
-              onClick={() => {
-                setIsDub(true);
-              }}
-            >
-              Dub
-            </button>
-            <button
-              className="btn btn-sm"
-              onClick={() => {
-                setIsDub(false);
-              }}
-            >
-              Sub
-            </button>
-            <button className="btn btn-sm ">Vidlink</button>
-            <button className="btn btn-sm ">Vidsrc</button>
+          <div className="flex gap-2 mb-4 justify-between">
+            <div className="">
+              <p className="text-left font-semibold">Idiomas</p>
+              <div className="flex gap-2">
+                <button
+                  className="btn btn-sm"
+                  onClick={() => {
+                    setIsDub(true);
+                  }}
+                >
+                  Eng
+                </button>
+                <button
+                  className="btn btn-sm"
+                  onClick={() => {
+                    setIsDub(false);
+                  }}
+                >
+                  Jap
+                </button>
+              </div>
+            </div>
+            <div>
+              <p className="font-semibold text-left">Servers</p>
+              <div className="flex gap-2">
+                <button className="btn btn-sm ">Vidlink</button>
+                <button className="btn btn-sm ">Vidsrc</button>
+              </div>
+            </div>
           </div>
         </section>
         <ul className="grid grid-cols-5 lg:grid-cols-10 gap-2 ">
