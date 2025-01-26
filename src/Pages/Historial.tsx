a85104a395e20c1d4d6736178b91e54b899c3120
Cesar ubuntu <cesarwamartinez@gmail.com>
2025-01-25 21:29:15 -0500

    se añade el dialog

--- src/Pages/Historial.tsx
+++ src/Pages/Historial.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Historial } from "../Types/Historial";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function History() {
   const navigate = useNavigate();
@@ -8,17 +9,52 @@ export default function History() {
     JSON.parse(localStorage.getItem("viendo") || "[]")
   );
 
-  if (logHistorial.length === 0 ) {
+  if (logHistorial.length === 0) {
     return (
-        <div className="w-full h-svh text-center content-center">
-            <h1 className="font-bold text-[2rem]">Tu array esta vacio "[]" XD</h1>
-        </div>
+      <div className="w-full h-svh text-center content-center">
+        <h1 className="font-bold text-[2rem]">Tu array esta vacio "[]" XD</h1>
+      </div>
     );
   }
 
   return (
     <main className="p-5 flex flex-col gap-3">
-      <h3 className="font-semibold text-lg">Estuviste viendo</h3>
+      <div className="flex justify-between">
+        <h3 className="font-semibold text-lg">Estuviste viendo</h3>
+        <button className="btn glass btn-sm">
+          <Icon icon="tabler:trash" width="20" height="20" />{" "}
+          {"Olvidar historial"}{" "}
+        </button>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button
+          className="btn btn-sm glass"
+          onClick={() => {
+            const modal = document.getElementById("my_modal_1") as HTMLDialogElement
+            if(modal && typeof modal.showModal === "function"){
+              modal.showModal()
+            }
+          }}
+        >
+          <Icon icon="tabler:trash" width="20" height="20" />{" "}
+          Olvidar historial
+        </button>
+        <dialog id="my_modal_1" className="modal ">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">⚠️ Esta acción no se puede deshacer. ¿Seguro que quieres eliminar tu historial?</h3>
+            <p className="py-4">
+            Si eliminas tu historial, perderás todos los animes que has guardado en tu lista. No podrás recuperar tu progreso ni los episodios que has marcado como vistos. ¡Asegúrate de estar completamente seguro antes de continuar!
+            </p>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn" onClick={() => {
+                  localStorage.clear()
+                }}>Estoy mas decidido que Goku en Namek</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
       <ul className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 ">
         {logHistorial.map((anime, index) => (
           <li
