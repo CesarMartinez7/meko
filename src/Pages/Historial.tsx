be1d668e46a6855b4586d2e363d848f4b2c22457
Cesar ubuntu <cesarwamartinez@gmail.com>
2025-01-25 21:34:05 -0500

    La logica de historial por ahora esta lista

--- src/Pages/Historial.tsx
+++ src/Pages/Historial.tsx
@@ -9,6 +9,13 @@ export default function History() {
     JSON.parse(localStorage.getItem("viendo") || "[]")
   );
 
+  const handleClickToDelete = () => {
+    const modal = document.getElementById("my_modal_1") as HTMLDialogElement;
+    if (modal && typeof modal.showModal === "function") {
+      modal.showModal();
+    }
+  };
+
   if (logHistorial.length === 0) {
     return (
       <div className="w-full h-svh text-center content-center">
@@ -21,35 +28,34 @@ export default function History() {
     <main className="p-5 flex flex-col gap-3">
       <div className="flex justify-between">
         <h3 className="font-semibold text-lg">Estuviste viendo</h3>
-        <button className="btn glass btn-sm">
-          <Icon icon="tabler:trash" width="20" height="20" />{" "}
-          {"Olvidar historial"}{" "}
-        </button>
         {/* Open the modal using document.getElementById('ID').showModal() method */}
-        <button
-          className="btn btn-sm glass"
-          onClick={() => {
-            const modal = document.getElementById("my_modal_1") as HTMLDialogElement
-            if(modal && typeof modal.showModal === "function"){
-              modal.showModal()
-            }
-          }}
-        >
-          <Icon icon="tabler:trash" width="20" height="20" />{" "}
-          Olvidar historial
+        <button className="btn btn-sm glass" onClick={handleClickToDelete}>
+          <Icon icon="tabler:trash" width="20" height="20" /> Olvidar historial
         </button>
         <dialog id="my_modal_1" className="modal ">
           <div className="modal-box">
-            <h3 className="font-bold text-lg">⚠️ Esta acción no se puede deshacer. ¿Seguro que quieres eliminar tu historial?</h3>
+            <h3 className="font-bold text-lg">
+              ⚠️ Esta acción no se puede deshacer. ¿Seguro que quieres eliminar
+              tu historial?
+            </h3>
             <p className="py-4">
-            Si eliminas tu historial, perderás todos los animes que has guardado en tu lista. No podrás recuperar tu progreso ni los episodios que has marcado como vistos. ¡Asegúrate de estar completamente seguro antes de continuar!
+              Si eliminas tu historial, perderás todos los animes que has
+              guardado en tu lista. No podrás recuperar tu progreso ni los
+              episodios que has marcado como vistos. ¡Asegúrate de estar
+              completamente seguro antes de continuar!
             </p>
             <div className="modal-action">
               <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
-                <button className="btn" onClick={() => {
-                  localStorage.clear()
-                }}>Estoy mas decidido que Goku en Namek</button>
+                <button
+                  className="btn btn-error"
+                  onClick={() => {
+                    localStorage.clear();
+                    location.reload();
+                  }}
+                >
+                  Eliminar
+                </button>
               </form>
             </div>
           </div>
